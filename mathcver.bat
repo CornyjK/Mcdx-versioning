3ed69d2cec3ab2c709b69a0d6d96532226b4b71a
CornyjK <cleverline1mc@gmail.com>
2018-07-16 20:29:07 +0200

    Major changes

--- mathcver.bat
+++ mathcver.bat
@@ -1,48 +1,48 @@
 @echo off
 chcp 65001 >nul 2>nul
 setlocal enableextensions enabledelayedexpansion
-set mathcver.version=0.1.0
+set mcdxver-version=0.1.0
 
-:7-zip.check
+:7-zip-check
 where 7z >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 goto 7-zip.install
-goto git.check
+IF %ERRORLEVEL% NEQ 0 goto 7-zip-install
+goto git-check
 
-:git.check
+:git-check
 where git >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 echo Warning: Git not found!
-goto mathcver.begin
+goto mcdxver-begin
 
-:7-zip.install
-IF 7-zip.installed 1 goto error.7-zip.verify
-echo Installing 7-zip...
-powershell wget -O %temp%\7z1805.exe https://www.7-zip.org/a/7z1805-x64.exe
+:7-zip-install
+IF 7-zip-installed 1 goto error-7-zip-verify
+echo Installing 7-zip-..
+powershell wget -O %temp%\7z1805.exe https://www.7-zip-org/a/7z1805-x64.exe
 %temp%\7z1805.exe /S /D="C:\Program Files\7-Zip"
-IF ERRORLEVEL 1 goto error.7-zip.install
-set 7-zip.installed 1
-goto 7-zip.check
+IF ERRORLEVEL 1 goto error-7-zip-install
+set 7-zip-installed 1
+goto 7-zip-check
 
-:mathcver.begin
-if "%1"=="help" goto mathcver.help
-if "%1"=="about" goto mathcver.about
-if "%1"=="pack" goto mathcver.pack
-if "%1"=="unpack" goto mathcver.unpack
-if "%1"=="" goto mathcver.help
-echo Unknown mathcver command!
-echo See 'mathcver help' to see available commands.
+:mcdxver-begin
+if "%1"=="help" goto mcdxver-help
+if "%1"=="about" goto mcdxver-about
+if "%1"=="pack" goto mcdxver-pack
+if "%1"=="unpack" goto mcdxver-unpack
+if "%1"=="" goto mcdxver-help
+echo Unknown mcdxver command!
+echo See 'mcdxver help' to see available commands.
 exit 1
 
-:mathcver.about
-echo Mathcad versioning tool %mathcver.version%
+:mcdxver-about
+echo Mathcad versioning tool %mcdxver-version%
 echo created by Kryštof Černý @CornyjK
 exit
 
-:mathcver.help
+:mcdxver-help
 rem cover help of some commands...
-if not "%2"=="" goto error.mathcver.help.na
-echo Mathcad versioning tool %mathcver.version%
+if not "%2"=="" goto error-mcdxver-help-na
+echo Mathcad versioning tool %mcdxver-version%
 echo.
-echo usage: mathcver ^<command^> ^<arguments^>
+echo usage: mcdxver ^<command^> ^<arguments^>
 echo.
 echo Available commands:
 echo about - about this script
@@ -51,44 +51,77 @@ echo unpack - unpacks mcdx sheet
 echo pack - packs mcdx sheet
 exit
 
-:mathcver.unpack
-rem unpack also sub-packages of sheet pages
-If Not Exist "%cd%\%2" goto error.mathcver.unpack.nonexistant
+:mcdxver-unpack
+set mcdxver-unpack-source=%2
 
-ren "%2" "%2" >nul 2>nul
-IF ERRORLEVEL 1 goto error.mathcver.unpack.used
+If Not Exist "%cd%\%mcdxver-unpack-source%" goto error-mcdxver-unpack-nonexistant
 
-7z x %2 -o%2 -y -aoa
-IF ERRORLEVEL 0 goto mathcver.unpack.success
+ren "%mcdxver-unpack-source%" "%mcdxver-unpack-source%" >nul 2>nul
+IF ERRORLEVEL 1 goto error-mcdxver-unpack-used
+
+SET mcdxver-unpack-target-tmp=%mcdxver-unpack-source:~0,-5%
+SET mcdxver-unpack-target=%mcdxver-unpack-target-tmp%.mcdxver
+echo %mcdxver-unpack-target%
+
+7z x %mcdxver-unpack-source% -o%mcdxver-unpack-target% -y -aoa
+
+IF ERRORLEVEL 0 (
+    echo Mcdx unpacked, now doing the files!
+    set mcdxver-unpack-pages-loop=1
+    set mcdxver-unpack-pages-page=0
+    goto mcdxver-unpack-pages-loop
+)
 echo An error occured!
 exit 1
 
-:mathcver.unpack.success
-echo Unpack successful!
+:mcdxver-unpack-pages-loop
+if "%mcdxver-unpack-pages-loop.active"=="1" (
+    7z x %2\ -o%2 -y -aoa
+    goto mcdxver-unpack-pages-loop
+)
 rem place verifying code here!
 exit
 
-:error.7-zip.install
+:error-7-zip-install
 echo Can't install 7-zip!
 echo Try restarting your machine or installing 7-zip manually.
 exit 1
 
-:error.7-zip.verify
+:error-7-zip-verify
 echo Can't detect 7-zip after successful installation!
 echo Try restarting your machine, installing 7-zip or adding 7-zip to path manually.
 exit 1
 
-:error.mathcver.unpack.used
+:error-mcdxver-unpack-used
 echo Something already uses desired file!
 echo Close all apps that use your sheet.
 exit 1
 
-:error.mathcver.unpack.nonexistant
+:error-mcdxver-unpack-nonexistant
 echo Desired file does not exist!
 echo Check your arguments.
 exit 1
 
-:error.mathcver.help.na
+:error-mcdxver-help-na
 echo Help about this command does not exist!
 echo There is probably nothing more to ask...
 exit 1
+
+:strlen <resultVar> <stringVar>
+rem stolen from: https://stackoverflow.com/questions/5837418/how-do-you-get-the-string-length-in-a-batch-file
+(   
+    setlocal EnableDelayedExpansion
+    set "s=!%~2!#"
+    set "len=0"
+    for %%P in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
+        if "!s:~%%P,1!" NEQ "" ( 
+            set /a "len+=%%P"
+            set "s=!s:~%%P!"
+        )
+    )
+)
+( 
+    endlocal
+    set "%~1=%len%"
+    exit /b
+)
