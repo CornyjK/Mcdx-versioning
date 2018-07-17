80c98f2be74b1db2d825a71109e6aeb9e2cb1a69
CornyjK <cleverline1mc@gmail.com>
2018-07-17 21:32:32 +0200

    Small cleanup

--- mcdxver.bat
+++ mcdxver.bat
@@ -5,20 +5,20 @@ set mcdxver_version=0.1.0
 
 :7-zip_check
 where 7z >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 goto 7-zip_install
+if %ERRORLEVEL% NEQ 0 goto 7-zip_install
 goto git_check
 
 :git_check
 where git >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 echo Warning: Git not found!
+if %ERRORLEVEL% NEQ 0 echo Warning: Git not found!
 goto mcdxver_begin
 
 :7-zip_install
-IF 7-zip_installed 1 goto error_7-zip_verify
-echo Installing 7-zip_..
+if 7-zip_installed 1 goto error_7-zip_verify
+echo Installing 7-zip...
 powershell wget -O %temp%\7z1805.exe https://www.7-zip_org/a/7z1805-x64.exe
 %temp%\7z1805.exe /S /D="C:\Program Files\7-Zip"
-IF ERRORLEVEL 1 goto error_7-zip_install
+if ERRORLEVEL 1 goto error_7-zip_install
 set 7-zip_installed 1
 goto 7-zip_check
 
@@ -27,6 +27,7 @@ if "%1"=="help" goto mcdxver_help
 if "%1"=="about" goto mcdxver_about
 if "%1"=="pack" goto mcdxver_pack
 if "%1"=="unpack" goto mcdxver_unpack
+if "%1"=="donate" goto mcdxver_donate
 if "%1"=="" goto mcdxver_help
 echo Unknown mcdxver command!
 echo See 'mcdxver help' to see available commands.
@@ -35,6 +36,11 @@ exit 1
 :mcdxver_about
 echo Mathcad versioning tool %mcdxver_version%
 echo created by Kryštof Černý @CornyjK
+echo "donate by ""mcdxver donate"""
+exit
+
+:mcdxver_donate
+start "" http://paypal.me/CornyjK
 exit
 
 :mcdxver_help
@@ -48,29 +54,29 @@ echo Available commands:
 echo about - about this script
 echo help - displays this help or help about command
 echo unpack - unpacks mcdx sheet
-echo pack - packs mcdx sheet
+echo pack - packs mcdx sheet - warning - may break your sheet
 exit
 
 :mcdxver_unpack
 set mcdxver_unpack_source=%2
 
-If Not Exist "%cd%\%mcdxver_unpack_source%" goto error_mcdxver_unpack_nonexistant
+if not exist "%cd%\%mcdxver_unpack_source%" goto error_mcdxver_unpack_nonexistant
 
 ren "%mcdxver_unpack_source%" "%mcdxver_unpack_source%" >nul 2>nul
-IF ERRORLEVEL 1 goto error_mcdxver_unpack_used
+if ERRORLEVEL 1 goto error_mcdxver_unpack_used
 
-SET mcdxver_unpack_target-tmp=%mcdxver_unpack_source:~0,-5%
-SET mcdxver_unpack_target=%mcdxver_unpack_target-tmp%.mcdxver
+set mcdxver_unpack_target-tmp=%mcdxver_unpack_source:~0,-5%
+set mcdxver_unpack_target=%mcdxver_unpack_target-tmp%.mcdxver
 
 7z x %mcdxver_unpack_source% -o%mcdxver_unpack_target% -y -aoa >nul 2>nul
 
-IF not ERRORLEVEL 0 goto error_undefined
+if not ERRORLEVEL 0 goto error_undefined
 echo Mcdx unpacked, now unpacking pages!
 set mcdxver_unpack_pages_page=0
 goto mcdxver_unpack_pages_loop
 
 :mcdxver_unpack_pages_loop
-If Exist "%cd%\%mcdxver_unpack_target%\mathcad\xaml\FlowDocument%mcdxver_unpack_pages_page%.XamlPackage" (
+if exist "%cd%\%mcdxver_unpack_target%\mathcad\xaml\FlowDocument%mcdxver_unpack_pages_page%.XamlPackage" (
     7z x %cd%\%mcdxver_unpack_target%\mathcad\xaml\FlowDocument%mcdxver_unpack_pages_page%.XamlPackage -o%cd%\%mcdxver_unpack_target%\mathcad\xaml\FlowDocument%mcdxver_unpack_pages_page% -y -aoa  >nul 2>nul
     set /a "mcdxver_unpack_pages_page+=1"
     goto mcdxver_unpack_pages_loop
@@ -109,22 +115,3 @@ exit 1
 echo Unknown error occured!
 echo You may contact author for help.
 exit 1
-
-:strlen <resultVar> <stringVar>
-rem stolen from: https://stackoverflow.com/questions/5837418/how-do-you-get-the-string-length-in-a-batch-file
-(   
-    setlocal EnableDelayedExpansion
-    set "s=!%~2!#"
-    set "len=0"
-    for %%P in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
-        if "!s:~%%P,1!" NEQ "" ( 
-            set /a "len+=%%P"
-            set "s=!s:~%%P!"
-        )
-    )
-)
-( 
-    endlocal
-    set "%~1=%len%"
-    exit /b
-)
