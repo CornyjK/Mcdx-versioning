4046726979792683815cd7270d047a2bc1b8a37b
CornyjK <cleverline1mc@gmail.com>
2018-07-17 21:17:59 +0200

    Add page unpacking + bugfix (->_)

--- mcdxver.bat
+++ mcdxver.bat
@@ -1,46 +1,46 @@
 @echo off
 chcp 65001 >nul 2>nul
 setlocal enableextensions enabledelayedexpansion
-set mcdxver-version=0.1.0
+set mcdxver_version=0.1.0
 
-:7-zip-check
+:7-zip_check
 where 7z >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 goto 7-zip-install
-goto git-check
+IF %ERRORLEVEL% NEQ 0 goto 7-zip_install
+goto git_check
 
-:git-check
+:git_check
 where git >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 echo Warning: Git not found!
-goto mcdxver-begin
+goto mcdxver_begin
 
-:7-zip-install
-IF 7-zip-installed 1 goto error-7-zip-verify
-echo Installing 7-zip-..
-powershell wget -O %temp%\7z1805.exe https://www.7-zip-org/a/7z1805-x64.exe
+:7-zip_install
+IF 7-zip_installed 1 goto error_7-zip_verify
+echo Installing 7-zip_..
+powershell wget -O %temp%\7z1805.exe https://www.7-zip_org/a/7z1805-x64.exe
 %temp%\7z1805.exe /S /D="C:\Program Files\7-Zip"
-IF ERRORLEVEL 1 goto error-7-zip-install
-set 7-zip-installed 1
-goto 7-zip-check
+IF ERRORLEVEL 1 goto error_7-zip_install
+set 7-zip_installed 1
+goto 7-zip_check
 
-:mcdxver-begin
-if "%1"=="help" goto mcdxver-help
-if "%1"=="about" goto mcdxver-about
-if "%1"=="pack" goto mcdxver-pack
-if "%1"=="unpack" goto mcdxver-unpack
-if "%1"=="" goto mcdxver-help
+:mcdxver_begin
+if "%1"=="help" goto mcdxver_help
+if "%1"=="about" goto mcdxver_about
+if "%1"=="pack" goto mcdxver_pack
+if "%1"=="unpack" goto mcdxver_unpack
+if "%1"=="" goto mcdxver_help
 echo Unknown mcdxver command!
 echo See 'mcdxver help' to see available commands.
 exit 1
 
-:mcdxver-about
-echo Mathcad versioning tool %mcdxver-version%
+:mcdxver_about
+echo Mathcad versioning tool %mcdxver_version%
 echo created by Kryštof Černý @CornyjK
 exit
 
-:mcdxver-help
+:mcdxver_help
 rem cover help of some commands...
-if not "%2"=="" goto error-mcdxver-help-na
-echo Mathcad versioning tool %mcdxver-version%
+if not "%2"=="" goto error_mcdxver_help_na
+echo Mathcad versioning tool %mcdxver_version%
 echo.
 echo usage: mcdxver ^<command^> ^<arguments^>
 echo.
@@ -51,60 +51,63 @@ echo unpack - unpacks mcdx sheet
 echo pack - packs mcdx sheet
 exit
 
-:mcdxver-unpack
-set mcdxver-unpack-source=%2
+:mcdxver_unpack
+set mcdxver_unpack_source=%2
 
-If Not Exist "%cd%\%mcdxver-unpack-source%" goto error-mcdxver-unpack-nonexistant
+If Not Exist "%cd%\%mcdxver_unpack_source%" goto error_mcdxver_unpack_nonexistant
 
-ren "%mcdxver-unpack-source%" "%mcdxver-unpack-source%" >nul 2>nul
-IF ERRORLEVEL 1 goto error-mcdxver-unpack-used
+ren "%mcdxver_unpack_source%" "%mcdxver_unpack_source%" >nul 2>nul
+IF ERRORLEVEL 1 goto error_mcdxver_unpack_used
 
-SET mcdxver-unpack-target-tmp=%mcdxver-unpack-source:~0,-5%
-SET mcdxver-unpack-target=%mcdxver-unpack-target-tmp%.mcdxver
+SET mcdxver_unpack_target-tmp=%mcdxver_unpack_source:~0,-5%
+SET mcdxver_unpack_target=%mcdxver_unpack_target-tmp%.mcdxver
 
-7z x %mcdxver-unpack-source% -o%mcdxver-unpack-target% -y -aoa
+7z x %mcdxver_unpack_source% -o%mcdxver_unpack_target% -y -aoa >nul 2>nul
 
-IF ERRORLEVEL 0 (
-    echo Mcdx unpacked, now doing the files!
-    set mcdxver-unpack-pages-loop-active=1
-    set mcdxver-unpack-pages-page=0
-    goto mcdxver-unpack-pages-loop
+IF not ERRORLEVEL 0 goto error_undefined
+echo Mcdx unpacked, now unpacking pages!
+set mcdxver_unpack_pages_page=0
+goto mcdxver_unpack_pages_loop
+
+:mcdxver_unpack_pages_loop
+If Exist "%cd%\%mcdxver_unpack_target%\mathcad\xaml\FlowDocument%mcdxver_unpack_pages_page%.XamlPackage" (
+    7z x %cd%\%mcdxver_unpack_target%\mathcad\xaml\FlowDocument%mcdxver_unpack_pages_page%.XamlPackage -o%cd%\%mcdxver_unpack_target%\mathcad\xaml\FlowDocument%mcdxver_unpack_pages_page% -y -aoa  >nul 2>nul
+    set /a "mcdxver_unpack_pages_page+=1"
+    goto mcdxver_unpack_pages_loop
 )
-echo An error occured!
-exit 1
-
-:mcdxver-unpack-pages-loop
-if "%mcdxver-unpack-pages-loop-active"=="1" (
-    If Exist "%cd%\%mcdxver-unpack-target%\mathcad\xaml\FlowDocument%mcdxver-unpack-pages-page%.XamlPackage" goto error-mcdxver-unpack-nonexistant
-    7z x %cd%\%mcdxver-unpack-target%\mathcad\xaml\FlowDocument%mcdxver-unpack-pages-page%.XamlPackage -o%cd%\%mcdxver-unpack-target%\mathcad\xaml\FlowDocument%mcdxver-unpack-pages-page% -y -aoa
-    goto mcdxver-unpack-pages-loop
-)
+set /a mcdxver_unpack_pages_pages=mcdxver_unpack_pages_page + 1
+echo Done, %mcdxver_unpack_pages_pages% pages total.
 rem place verifying code here!
 exit
 
-:error-7-zip-install
+:error_7-zip_install
 echo Can't install 7-zip!
 echo Try restarting your machine or installing 7-zip manually.
 exit 1
 
-:error-7-zip-verify
+:error_7-zip_verify
 echo Can't detect 7-zip after successful installation!
 echo Try restarting your machine, installing 7-zip or adding 7-zip to path manually.
 exit 1
 
-:error-mcdxver-unpack-used
+:error_mcdxver_unpack_used
 echo Something already uses desired file!
 echo Close all apps that use your sheet.
 exit 1
 
-:error-mcdxver-unpack-nonexistant
+:error_mcdxver_unpack_nonexistant
 echo Desired file does not exist!
 echo Check your arguments.
 exit 1
 
-:error-mcdxver-help-na
+:error_mcdxver_help_na
 echo Help about this command does not exist!
 echo There is probably nothing more to ask...
+exit 1
+
+:error_undefined
+echo Unknown error occured!
+echo You may contact author for help.
 exit 1
 
 :strlen <resultVar> <stringVar>
